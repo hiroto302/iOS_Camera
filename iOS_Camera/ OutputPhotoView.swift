2ea1c9e1a3d4eae8e3b28ac9f085231a3018b60b
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-16 21:43:29 +0900

    OutputPhotoView_画像モノクロームフィルタ機能_実装
    撮影した画像をモノクロで出力して表示する機能追加

--- iOS_Camera/ OutputPhotoView.swift
+++ iOS_Camera/ OutputPhotoView.swift
@@ -1,15 +1,17 @@
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
-// 撮影した画像を表示する View
 struct OutputPhotoView: View {
     // 撮影された画像を保持する変数
     @Binding var capturedImage: UIImage?
     @Environment(\.presentationMode) private var presentationMode
+    // モノクロームに加工した画像を保持する変数
+    @State private var monochromeImage: UIImage?
 
     var body: some View {
         ZStack {
             // 撮影された画像を全画面で表示
-            if let image = capturedImage {
+            if let image = translateColorMonochrome(from: capturedImage!) {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFill()
@@ -35,5 +37,31 @@ struct OutputPhotoView: View {
             }
         }
     }
+
+    // モノクローム加工したUI UIImage を出力
+    func translateColorMonochrome(from image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        let ciContext = CIContext(options: nil)
+
+        // CIImageを使用した画像編集処理
+        let filteredCIImage = colorMonochrome(inputImage: ciImage)
+
+        guard let cgImage = ciContext.createCGImage(filteredCIImage, from: filteredCIImage.extent) else { return nil }
+        let result = UIImage(cgImage: cgImage, scale: image.scale, orientation: image.imageOrientation)
+
+        return result
+    }
+
+    // モノクローム加工処理した CIImange を出力
+    func colorMonochrome(inputImage: CIImage) -> CIImage {
+        let colorMonochromeFilter = CIFilter.colorMonochrome()
+        colorMonochromeFilter.inputImage = inputImage
+        colorMonochromeFilter.color = CIColor(red: 0.5, green: 0.5, blue: 0.5)
+        colorMonochromeFilter.intensity = 1
+        return colorMonochromeFilter.outputImage!
+    }
+
+
 }
 
