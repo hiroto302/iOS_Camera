703f95eb897cd83cff43d92c5fba45a00f5f7f1c
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-16 20:28:54 +0900

    Initial Commit

---
+++ iOS_Camera/iOS_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CameraApp.swift
+//  iOS_Camera
+//
+//  Created by hiroto taniguchi on 2024/03/16.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
